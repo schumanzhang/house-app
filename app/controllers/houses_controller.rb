01f5c4936110a85f564bd94d325908176d2f20dc
Schuman Zhang <sz@proadviser.com.au>
2022-01-15 20:39:26 +1100

    wip

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,9 @@
+class HousesController < ApplicationController
+
+    def index
+        @houses = House.all
+    end
+
+    
+
+end
---
+++ app/controllers/inspections_controller.rb
@@ -0,0 +1,10 @@
+class InspectionsController < ApplicationController
+
+    def new
+    end
+
+    # POST /inspections
+    def create
+        
+    end
+end
